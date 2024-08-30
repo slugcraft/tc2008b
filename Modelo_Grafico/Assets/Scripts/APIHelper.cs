854b1c54714b62456a37e45db703cc74ef132064
Emiliano-Valdivia <a01276258@tec.mx>
2024-08-30 15:44:52 -0600

    Casa decorada y scripts creados vacios

---
+++ Modelo_Grafico/Assets/Scripts/APIHelper.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; //Para la clase JsonUtility
+using System.Net;
+using System.IO;
+
+public static class APIHelper
+{
+    public static Agent UpdateGame()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.chucknorris.io/jokes/random");
+
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+
+        string json = reader.ReadToEnd();
+
+        return JsonUtility.FromJson<Agent>(json);
+    }
+}
---
+++ Modelo_Grafico/Assets/Scripts/Agent.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+public class Agent
+{
+    public string[] categories;
+    public string created_at;
+    public string icon_url;
+    public string id;
+    public string updated_at;
+    public string url;
+    public string value;
+}
