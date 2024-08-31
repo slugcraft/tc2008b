132eb8f5bf0636c9d9c64ee76b617e70216a1ecd
Emiliano-Valdivia <a01276258@tec.mx>
2024-08-30 19:42:36 -0600

    Script Turno Creado

---
+++ Modelo_Grafico/Assets/Scripts/Turno.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+public class Turn
+{
+    public string[] categories;
+    public string created_at;
+    public string icon_url;
+    public string id;
+    public string updated_at;
+    public string url;
+    public string value;
+}
